bea392ad6a1b6020dbf526d1c09f246173abe3e3
Derrick McMillen <derrick@repl.it>
2021-03-16 22:12:39 +0000

    Remove solution code

--- TextTranslator.java
+++ TextTranslator.java
@@ -9,27 +9,16 @@ public class TextTranslator {
   }};
 
   public String convertTerm(String term) {
-    for (TextSlang slang : slangDictionary) {
-      if (slang.getSlang().equals(term)) {
-        return slang.getDefinition();
-      }
-    }
-
+   
+    // TODO: part a implementation
 
     return term;
   }
 
   public String expandText(String withSlang) {
 
-    String expanded = withSlang;
+    // TODO: part b implementation
 
-    // Unbounded compexity is possible with recursive definitions.
-    // Probably not the point of this problem.
-
-    for (TextSlang slang : slangDictionary) {
-      expanded = expanded.replaceAll(slang.getSlang(), slang.getDefinition());
-    }
-
-    return expanded;
+    return withSlang;
   }
 }
